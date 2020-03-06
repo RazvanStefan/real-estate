f652a47e534b969d3b70f066eb7676572495d2ce
RedirecT <43145170+RazvanStefan@users.noreply.github.com>
2020-03-06 12:19:22 +0200

    [RO] Translated UI.

--- resources/views/agent/properties/create.blade.php
+++ resources/views/agent/properties/create.blade.php
@@ -18,7 +18,7 @@
 
                 <div class="col s12 m9">
                     <div class="agent-content">
-                        <h4 class="agent-title">CREATE PROPERTY</h4>
+                        <h4 class="agent-title">ADAUGA ANUNT</h4>
 
                         <form action="{{route('agent.properties.store')}}" method="POST" enctype="multipart/form-data">
                             @csrf
@@ -26,43 +26,43 @@
                                 <div class="input-field col s12">
                                     <i class="material-icons prefix">title</i>
                                     <input id="title" name="title" type="text" class="validate" data-length="200">
-                                    <label for="title">Title</label>
+                                    <label for="title">Titlu</label>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="input-field col s6">
                                     <i class="material-icons prefix">monetization_on</i>
                                     <input id="price" name="price" type="number" class="validate">
-                                    <label for="price">Price</label>
+                                    <label for="price">Pret</label>
                                 </div>
                                 <div class="input-field col s6">
                                     <i class="material-icons prefix">business</i>
                                     <input id="area" name="area" type="number" class="validate">
-                                    <label for="area">Floor Area</label>
+                                    <label for="area">Suprafata</label>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="input-field col s6">
                                     <i class="material-icons prefix">airline_seat_flat</i>
                                     <input id="bedroom" name="bedroom" type="number" class="validate">
-                                    <label for="bedroom">Bedroom</label>
+                                    <label for="bedroom">Dormitoare</label>
                                 </div>
                                 <div class="input-field col s6">
                                     <i class="material-icons prefix">event_seat</i>
                                     <input id="bathroom" name="bathroom" type="number" class="validate">
-                                    <label for="bathroom">Bathroom</label>
+                                    <label for="bathroom">Bai</label>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="input-field col s4">
                                     <i class="material-icons prefix">location_city</i>
                                     <input id="city" name="city" type="text" class="validate">
-                                    <label for="city">City</label>
+                                    <label for="city">Oras</label>
                                 </div>
                                 <div class="input-field col s8">
                                     <i class="material-icons prefix">account_balance</i>
                                     <textarea id="address" name="address" class="materialize-textarea"></textarea>
-                                    <label for="address">Address</label>
+                                    <label for="address">Adresa</label>
                                 </div>
                             </div>
                             <div class="row">
@@ -82,63 +82,63 @@
                                     <p>
                                         <label>
                                             <input type="checkbox" name="featured" class="filled-in" checked="checked" />
-                                            <span>Featured</span>
+                                            <span>utilitati</span>
                                         </label>
                                     </p>
                                 </div>
                                 <div class="input-field col s9">
                                     <i class="material-icons prefix">mode_edit</i>
                                     <textarea id="description" name="description" class="materialize-textarea"></textarea>
-                                    <label for="description">Description</label>
+                                    <label for="description">Descriere</label>
                                 </div>
                             </div>
 
                             <div class="row">
                                 <div class="col s3">
-                                    <label class="label-custom" for="type">Property Type</label>
+                                    <label class="label-custom" for="type">Tipul anuntului</label>
                                     <p>
                                         <label>
                                             <input class="with-gap" name="type" value="house" type="radio"  />
-                                            <span>Sale</span>
+                                            <span>Vanzare</span>
                                         </label>
                                     <p>
                                     </p>
                                         <label>
                                             <input class="with-gap" name="type" value="apartment" type="radio"  />
-                                            <span>Rent</span>
+                                            <span>Inchiriere</span>
                                         </label>
                                     </p>
                                 </div>
                                 <div class="col s3">
-                                    <label class="label-custom" for="purpose">Property Purpose</label>
+                                    <label class="label-custom" for="purpose">Tipul imobilului</label>
                                     <p>
                                         <label>
                                             <input class="with-gap" name="purpose" value="sale" type="radio"  />
-                                            <span>House</span>
+                                            <span>Casa</span>
                                         </label>
                                     <p>
                                     </p>
                                         <label>
                                             <input class="with-gap" name="purpose" value="rent" type="radio"  />
-                                            <span>Apartment</span>
+                                            <span>Apartament</span>
                                         </label>
                                     </p>
                                 </div>
                                 <div class="input-field col s6">
                                     <select multiple name="features[]">
-                                        <option value="" disabled selected>Choose Features</option>
+                                        <option value="" disabled selected>Selecteaza Utilitati</option>
                                         @foreach($features as $feature)
                                             <option value="{{ $feature->id }}">{{ $feature->name }}</option>
                                         @endforeach
                                     </select>
-                                    <label class="label-custom">Select Features</label>
+                                    <label class="label-custom">Selecteaza Utilitati</label>
                                 </div>
                             </div>
 
                             <div class="row">
                                 <div class="file-field input-field col s12">
                                     <div class="btn indigo">
-                                        <span>Featured Image</span>
+                                        <span>Imagine de coperta</span>
                                         <input type="file" name="image">
                                     </div>
                                     <div class="file-path-wrapper">
@@ -164,14 +164,14 @@
                                 <div class="input-field col s12">
                                     <i class="material-icons prefix">voice_chat</i>
                                     <input id="video" name="video" type="text" class="validate">
-                                    <label for="video">Youtube Link</label>
+                                    <label for="video">Youtube</label>
                                 </div>
                             </div>
 
                             <div class="row">
                                 <div class="file-field input-field col s12">
                                     <div class="btn indigo">
-                                        <span>Floor Plan</span>
+                                        <span>Schita etajului</span>
                                         <input type="file" name="floor_plan">
                                     </div>
                                     <div class="file-path-wrapper">
@@ -184,19 +184,19 @@
                                 <div class="input-field col s12">
                                     <i class="material-icons prefix">place</i>
                                     <textarea id="nearby" name="nearby" class="materialize-textarea"></textarea>
-                                    <label for="nearby">Nearby</label>
+                                    <label for="nearby">Vecinatati</label>
                                 </div>
                             </div>
 
                             <div class="row">
                                 <div class="file-field input-field col s12">
                                     <div class="btn indigo">
-                                        <span>Upload Gallery Images</span>
+                                        <span>Adauga Galeria de Imagini</span>
                                         <input type="file" name="gallaryimage[]" multiple>
-                                        <span class="helper-text" data-error="wrong" data-success="right">Upload one or more images</span>
+                                        <span class="helper-text" data-error="wrong" data-success="right">Agaudati una sau mai multe imagini</span>
                                     </div>
                                     <div class="file-path-wrapper">
-                                        <input class="file-path validate" type="text" placeholder="Upload one or more images">
+                                        <input class="file-path validate" type="text" placeholder="Adaugati una sau mai multe imagini">
                                     </div>
                                 </div>
                             </div>
@@ -204,7 +204,7 @@
                             <div class="row">
                                 <div class="col s12 m-t-30">
                                     <button class="btn waves-effect waves-light btn-large indigo darken-4" type="submit">
-                                        Submit
+                                        Adauga
                                         <i class="material-icons right">send</i>
                                     </button>
                                 </div>
