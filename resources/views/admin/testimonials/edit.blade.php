dd4c2cf9b6ba8a88c47cd1aa99342cfea5de353f
RedirecT <43145170+RazvanStefan@users.noreply.github.com>
2020-03-06 13:12:17 +0200

    [RO] Translated UI.

--- resources/views/admin/testimonials/edit.blade.php
+++ resources/views/admin/testimonials/edit.blade.php
@@ -13,7 +13,7 @@
     <div class="block-header">
         <a href="{{route('admin.testimonials.index')}}" class="waves-effect waves-light btn btn-danger right m-b-15">
             <i class="material-icons left">arrow_back</i>
-            <span>BACK</span>
+            <span>INAPOI</span>
         </a>
     </div>
 
@@ -21,7 +21,7 @@
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
             <div class="card">
                 <div class="header bg-indigo">
-                    <h2>EDIT TESTIMONIAL</h2>
+                    <h2>EDITEAZA TESTIMONIAL</h2>
                 </div>
                 <div class="body">
                     <form action="{{route('admin.testimonials.update',$testimonial->id)}}" method="POST" enctype="multipart/form-data">
@@ -44,7 +44,7 @@
                             <input type="file" name="image" id="testimonial-image-input-edit" style="display:none;">
                             <button type="button" class="btn bg-grey btn-sm waves-effect m-t-15" id="testimonial-image-btn-edit">
                                 <i class="material-icons">image</i>
-                                <span>UPLOAD IMAGE</span>
+                                <span>ADAUGA IMAGINE</span>
                             </button>
                         </div>
 
@@ -57,7 +57,7 @@
 
                         <button type="submit" class="btn btn-indigo btn-lg m-t-15 waves-effect">
                             <i class="material-icons">update</i>
-                            <span>Update</span>
+                            <span>Actualizeaza</span>
                         </button>
 
                     </form>
