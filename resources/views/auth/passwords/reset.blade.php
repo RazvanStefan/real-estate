403f0fb0e9bea6a14ae329fcb854b9a64538891a
RedirecT <43145170+RazvanStefan@users.noreply.github.com>
2020-03-06 11:32:45 +0200

    [RO] Translated UI.

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col s12 m6 offset-m3">
             <div class="card">
-                <h4 class="center indigo-text p-t-30">{{ __('Reset Password') }}</h4>
+                <h4 class="center indigo-text p-t-30">{{ __('Reseteaza Parola') }}</h4>
 
                 <div class="p-20">
                     <form method="POST" action="{{ route('password.request') }}">
@@ -15,7 +15,7 @@
 
                         <div class="row">
                             <div class="input-field col s12">
-                                <label for="email">{{ __('E-Mail Address') }}</label>
+                                <label for="email">{{ __('E-Mail') }}</label>
                                 <input id="email" type="email" class="{{ $errors->has('email') ? 'is-invalid' : '' }}" name="email" value="{{ $email ?? old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
@@ -28,7 +28,7 @@
 
                         <div class="row">
                             <div class="input-field col s12">
-                                <label for="password">{{ __('Password') }}</label>
+                                <label for="password">{{ __('Parola') }}</label>
                                 <input id="password" type="password" class="{{ $errors->has('password') ? 'is-invalid' : '' }}" name="password" required>
 
                                 @if ($errors->has('password'))
@@ -41,7 +41,7 @@
 
                         <div class="row">
                             <div class="input-field col s12">
-                                <label for="password-confirm">{{ __('Confirm Password') }}</label>
+                                <label for="password-confirm">{{ __('Confirma Parola') }}</label>
                                 <input id="password-confirm" type="password" name="password_confirmation" required>
                             </div>
                         </div>
@@ -49,7 +49,7 @@
                         <div class="row">
                             <div class="input-field col s12">
                                 <button type="submit" class="waves-effect waves-light btn indigo">
-                                    {{ __('Reset Password') }}
+                                    {{ __('Reseteaza Parola') }}
                                 </button>
                             </div>
                         </div>
